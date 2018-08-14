af96c35e0ca08d00355012d6141b388ce52cc5cd
Brad McCollum <btmcco8@gmail.com>
2018-08-14 14:42:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,12 +28,12 @@ end
 
 def swap_elements(array) # needs to switch index 1 and 2
   array.sort do |a, b|
-    if a == b
+    if a == b 
       0
-    elsif array.index[a] == 1 && array.index[b] == 2
-      array.index[a] + 1
-    elsif array.index[a] == 2 && array.index[b] == 1
-      array.index[a] - 1
+    elsif array[a] > array[b]
+      array[a], array[b] = array[b], array[a]
     end
   end
+  
+      
 end
